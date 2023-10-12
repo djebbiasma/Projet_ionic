e6a2ac5f6c56ad206a6fdcbd00c5741ea5127be6
Asma <asmadjebbi123@gmail.com>
2023-10-12 13:30:03 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage { // Mettez à jour le nom de la classe
+  annonces = [
+    {
+      titre: 'Annonce 1',
+      description: 'Description de l\'annonce 1ggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg',
+    },
+    {
+      titre: 'Annonce 2',
+      description: 'Description de l\'annonce 2ghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh',
+    },
+    // Ajoutez d'autres annonces ici
+  ];
+  annoncesFiltrees = this.annonces; // Créez une copie des annonces pour la recherche
 
-  constructor() {}
-
+  onSearch(event: CustomEvent) {
+    const texteRecherche = event.detail.value.toLowerCase();
+    this.annoncesFiltrees = this.annonces.filter(annonce => {
+      return annonce.titre.toLowerCase().includes(texteRecherche);
+    });
+  }
+  nouvelleAnnonce() {
+    // Implement the logic for creating a new announcement or navigate to a new announcement creation page.
+  }
+  
 }
