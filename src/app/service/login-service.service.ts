9ef0aa23b0ad6db283cf7b0c5e689d26780c1c9b
Torkhani Ala <alatork2@gmail.com>
2023-10-15 23:36:58 +0100

    login commit

--- src/app/service/login-service.service.ts
+++ src/app/service/login-service.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class LoginService {
-  private apiUrl = 'http://localhost:5000/user'; // Remplacez par l'URL de votre API Node.js
+  private apiUrl = 'http://localhost:5000/user'; 
 
   constructor(private http: HttpClient) {}
 
@@ -16,7 +16,6 @@ export class LoginService {
     return this.http.post(`${this.apiUrl}/login`, loginData);
   }
 
-  // Vous pouvez ajouter d'autres méthodes d'authentification ici (inscription, déconnexion, etc.).
 }
 
 
